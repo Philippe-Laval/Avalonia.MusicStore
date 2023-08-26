ddae452e5abdfeca71105a2263c773215fb16736
Philippe Laval <philippe.laval.1968@laposte.net>
2023-08-26 16:44:14 +0200

    First step : Command

--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace Avalonia.MusicStore.ViewModels;
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace Avalonia.MusicStore.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public ICommand BuyMusicCommand { get; }
+    
+    public MainWindowViewModel()
+    {
+        BuyMusicCommand = ReactiveCommand.Create(() =>
+        {
+            // Code here will be executed when the button is clicked.
+            Debug.WriteLine("test");
+        });
+    }
+    
 }
